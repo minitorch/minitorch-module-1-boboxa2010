8088a144bb8005767ba00bb3b65477b58da21b05
Ivan Boboshko <ivanboboshko888@gmail.com>
2025-09-30 21:15:34 +0300

    add 01 solution

--- tests/test_operators.py
+++ tests/test_operators.py
@@ -107,14 +107,16 @@ def test_sigmoid(a: float) -> None:
     * It crosses 0 at 0.5
     * It is  strictly increasing.
     """
-    raise NotImplementedError("Need to include this file from past assignment.")
+    assert_close(1 - sigmoid(a), sigmoid(-a))
+    assert_close(sigmoid(0.0), 0.5)
 
 
 @pytest.mark.task0_2
 @given(small_floats, small_floats, small_floats)
 def test_transitive(a: float, b: float, c: float) -> None:
     "Test the transitive property of less-than (a < b and b < c implies a < c)"
-    raise NotImplementedError("Need to include this file from past assignment.")
+    if a < b and b < c:
+        assert a < c
 
 
 @pytest.mark.task0_2
@@ -123,7 +125,7 @@ def test_symmetric() -> None:
     Write a test that ensures that :func:`minitorch.operators.mul` is symmetric, i.e.
     gives the same value regardless of the order of its input.
     """
-    raise NotImplementedError("Need to include this file from past assignment.")
+    assert_close(mul(1, 2), mul(2, 1))
 
 
 @pytest.mark.task0_2
@@ -132,7 +134,7 @@ def test_distribute() -> None:
     Write a test that ensures that your operators distribute, i.e.
     :math:`z \times (x + y) = z \times x + z \times y`
     """
-    raise NotImplementedError("Need to include this file from past assignment.")
+    assert_close(mul(1.0, add(2.0, 3.0)), add(mul(1.0, 2.0), mul(1.0, 3.0)))
 
 
 @pytest.mark.task0_2
@@ -140,7 +142,7 @@ def test_other() -> None:
     """
     Write a test that ensures some other property holds for your functions.
     """
-    raise NotImplementedError("Need to include this file from past assignment.")
+    assert_close(add(1, 2), add(2, 1))
 
 
 # ## Task 0.3  - Higher-order functions
@@ -168,7 +170,7 @@ def test_sum_distribute(ls1: List[float], ls2: List[float]) -> None:
     Write a test that ensures that the sum of `ls1` plus the sum of `ls2`
     is the same as the sum of each element of `ls1` plus each element of `ls2`.
     """
-    raise NotImplementedError("Need to include this file from past assignment.")
+    assert_close(sum(addLists(ls1, ls2)), sum(ls1) + sum(ls2))
 
 
 @pytest.mark.task0_3
